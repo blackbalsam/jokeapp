473bf45f31e5e1119e3bf89655d3d2b1d4696346
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-13 14:25:29 +0300

    Created service to return joke string from class guru.springframework.norris.chuck.ChuckNorrisQuotes.getRandomQuote()

---
+++ src/main/java/com/example/joke/jokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.example.joke.jokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/example/joke/jokeapp/services/impl/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.joke.jokeapp.services.impl;
+
+import com.example.joke.jokeapp.services.JokeService;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
